4d7bf6b644f688b130d613ac9e1a0f2ffbfa8c1d
Alexander Kvast <akvast@corp.local>
2018-02-25 21:47:37 +0300

    Remove OBJ folder;

--- app/src/main/obj/local/armeabi-v7a/objs/protobuf-cpp-full-ndk/src/google/protobuf/io/gzip_stream.o.d
+++
@@ -1,2 +0,0 @@
-/home/alexander/dev/chat-android/app/src/main/obj/local/armeabi-v7a/objs/protobuf-cpp-full-ndk/src/google/protobuf/io/gzip_stream.o: \
- /home/alexander/dev/chat-android/app/src/main/jni/protobuf/src/src/google/protobuf/io/gzip_stream.cc
